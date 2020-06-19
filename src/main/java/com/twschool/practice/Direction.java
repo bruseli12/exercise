037b9190d32dd1b8ea46c230191263317fed7ffa
bruseli12 <lihuadeng@zybank.com.cn>
2020-06-19 10:51:22 +0800

    finished by lihuadeng

--- src/main/java/com/twschool/practice/Direction.java
+++ src/main/java/com/twschool/practice/Direction.java
@@ -1,5 +1,47 @@
 package com.twschool.practice;
 
 public enum Direction {
+    E {
+        @Override
+        public Enum turnleft() {
+            return N;
+        }
 
+        @Override
+        public Enum turnRight() {
+            return S;
+        }
+    },S {
+        @Override
+        public Enum turnleft() {
+            return W;
+        }
+
+        @Override
+        public Enum turnRight() {
+            return null;
+        }
+    },W {
+        @Override
+        public Enum turnleft() {
+            return N;
+        }
+
+        @Override
+        public Enum turnRight() {
+            return null;
+        }
+    },N {
+        @Override
+        public Enum turnleft() {
+            return E;
+        }
+
+        @Override
+        public Enum turnRight() {
+            return null;
+        }
+    };
+   public abstract Enum turnleft();
+   public  abstract Enum turnRight();
 }

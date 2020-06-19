c8c397e8404597a31e1c1ba14df3730c00788142
bruseli12 <lihuadeng@zybank.com.cn>
2020-06-19 16:39:32 +0800

    finish by lihuadeng should_return_0A0B_when_check_given_gameanswer_1234_useranswer_1123

--- src/main/java/com/twschool/practice/GuestNumber.java
+++ src/main/java/com/twschool/practice/GuestNumber.java
@@ -6,6 +6,14 @@ public class GuestNumber {
         int useranswersize = useranswer.length;
         if (gameanswersize != useranswersize)
             return "0A0B";
+        for(int i=0;i<useranswersize;i++){
+            for(int j=0;j<useranswersize;j++){
+                if(i==j)
+                    continue;
+                if(useranswer[i]==useranswer[j])
+                    return  "0A0B";
+            }
+        }
 
          return "4A0b";
     }
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -8,10 +8,23 @@ public class GuessNumberTest {
 
 
     @Test
-    public void should_return_4A0B_when_check_given_gameanswer_1234_useranswer_123(){
+    public void should_return_0A0B_when_check_given_gameanswer_1234_useranswer_123(){
         //given
         int[] gameanswer=new int[]{1,2,3,4};
         int[] useranswer=new int[]{1,2,3};
+        //when
+        String result=GuestNumber.check(gameanswer,useranswer);
+        //then
+        assertEquals(result,"0A0B");
+    }
+
+
+
+    @Test
+    public void should_return_0A0B_when_check_given_gameanswer_1234_useranswer_1123(){
+        //given
+        int[] gameanswer=new int[]{1,2,3,4};
+        int[] useranswer=new int[]{1,1,2,3};
         //when
         String result=GuestNumber.check(gameanswer,useranswer);
         //then
